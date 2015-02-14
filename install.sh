42bba6ab09574afbb599a88dce8001cfee059b08
Bram Harmsen <bramharmsen@gmail.com>
2015-02-14 17:08:16 +0100

    updated install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
-fi
+fi  
 
 # install usefull terminal packages
-sudo apt-get install -y dircolors cowsay toilet forutune pygmentize htop
+sudo apt-get install -y dircolors cowsay toilet fortune python-pygments htop
