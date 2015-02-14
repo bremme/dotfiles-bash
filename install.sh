270db5162285e4c9d3372753ce7d2450e37a56cb
Bram Harmsen <bramharmsen@gmail.com>
2015-02-14 17:03:19 +0100

    added nanorc and nano syntax highlighting

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# check root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install usefull terminal packages
+sudo apt-get install -y dircolors cowsay toilet forutune pygmentize htop
